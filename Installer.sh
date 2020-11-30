9e55312ab0dfb19ca032a646e392e217a5b9d916
ivanjrt <44326428+ivanjrt@users.noreply.github.com>
2020-11-29 19:37:27 -0600

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -2,7 +2,7 @@
 #some of this was compiled from source: https://linuxize.com/post/how-to-install-python-3-8-on-debian-10/ and others
 
 sudo apt update
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y
 curl -O https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz
 tar -xf Python-3.9.0.tar.xz
 cd Python-3.9.0
